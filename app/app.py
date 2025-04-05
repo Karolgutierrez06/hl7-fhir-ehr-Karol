d6fd0e0c79981dd0b0fcd05cc9a8f67222dc2dd9
Karolgutierrez06 <KAROLM.GUTIERREZ@UROSARIO.EDU.CO>
2025-04-05 11:21:49 -0500

    .

--- app/app.py
+++ app/app.py
@@ -34,6 +34,15 @@ async def get_patient_by_identifier(system: str, value: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
+@app.get("/service-request/{service_request_id}", response_model=dict)
+async def get_service_request(service_request_id: str):
+    # Llama a la función auxiliar que obtiene la solicitud de servicio
+    service_request = read_service_request(service_request_id)
+    if service_request:
+        return service_request
+    else:
+        raise HTTPException(status_code=404, detail="Solicitud de servicio no encontrada")
+
 
 @app.post("/patient", response_model=dict)
 async def add_patient(request: Request):
--- app/controlador/PatientCrud.py
+++ app/controlador/PatientCrud.py
@@ -46,3 +46,20 @@ def GetPatientByIdentifier(patientSystem, patientValue):
     except Exception as e:
         print("Error in GetPatientByIdentifier:", e)
         return "notFound", None
+
+def read_service_request(service_request_id: str) -> dict:
+    """
+    Recupera una solicitud de servicio a partir de su ID.
+    """
+    try:
+        query = {"_id": ObjectId(service_request_id)}
+    except Exception as e:
+        print("Error al convertir el ID:", e)
+        return None
+
+    service_request = service_requests_collection.find_one(query)
+    if service_request:
+        service_request["_id"] = str(service_request["_id"])
+        return service_request
+    else:
+        return None
